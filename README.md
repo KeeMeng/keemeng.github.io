31866b319f1fdc61cd6f2ed5b096ff9e544a4b45
KeeMeng <KeeMeng@users.noreply.github.com>
2021-04-13 14:40:22 +0800

    universal dark mode

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Some of the pages I made include:
 - SI Prefixes Converter: Converts units eg kilo to milli
 - Molar Mass Calculator: Calculates the molecular/formula mass of the input molecule
 - Cubes: A way to create and visualize cubes, and you can rotate them in 3d space
+- Screensavers: Input links of some images to customize a few screensavers that I made
--- cubes.js
+++ cubes.js
@@ -7,18 +7,49 @@ if (temp > 0) {n = temp;}
 var matrix = [];
 var input_count = 0;
 
-document.body.classList.toggle("lightmode");
-var dark = false;
+var dark = localStorage.getItem("mode");
 function dark_mode() {
-	dark = !dark;
-	document.body.classList.toggle("darkmode");
-	document.body.classList.toggle("lightmode");
-	if (dark) {
+	if (dark == "light") {
+		dark = "dark";
+		localStorage.setItem("mode", "dark");
+	}
+	else if (dark == "dark") {
+		dark = "light";
+		localStorage.setItem("mode", "light");
+	}
+	if (dark == "dark") {
+		document.getElementById("mode").innerHTML = "Light Mode";
 		document.getElementById("wrapper").style.backgroundColor = "#222";
-	} else {
+	}
+	else if (dark == "light") {
+		document.getElementById("mode").innerHTML = "Dark Mode";
 		document.getElementById("wrapper").style.backgroundColor = "#EEE";
 	}
+	document.body.classList.toggle("darkmode");
+	document.body.classList.toggle("lightmode");
 }
+
+function load() {
+	document.body.style.transitionDuration = "0s";
+	document.getElementById("wrapper").style.transitionDuration = "0s";
+	if (dark == "dark") {
+		document.body.classList.toggle("darkmode");
+		document.getElementById("mode").innerHTML = "Light Mode";
+		document.getElementById("wrapper").style.backgroundColor = "#222";
+	}
+	else if (dark == "light") {
+		document.body.classList.toggle("lightmode");
+		document.getElementById("mode").innerHTML = "Dark Mode";
+		document.getElementById("wrapper").style.backgroundColor = "#EEE";
+	}
+	setTimeout(add_transition,500);
+}
+
+function add_transition() {
+	document.body.style.transitionDuration = "0.5s";
+	document.getElementById("wrapper").style.transitionDuration = "0.5s";
+}
+
 
 function set_matrix() {
 	matrix = [];
