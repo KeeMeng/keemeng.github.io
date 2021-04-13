bb1020f73bb57b3acf8c92c7be37b89840822f33
KeeMeng <KeeMeng@users.noreply.github.com>
2021-04-14 00:25:22 +0800

    even better index.html with animations

--- index.js
+++ index.js
@@ -46,39 +46,50 @@ function dark_mode() {
 	if (dark == "dark") {
 		document.getElementById("mode").innerHTML = "Light Mode";
 		var projects = document.getElementsByClassName("projects");
+		var texts = document.getElementsByClassName("hovertext");
 		for (var i = 0; i < projects.length; i++) {
+			projects[i].style.transitionDuration = "0.5s";
 			projects[i].style.backgroundColor = "#102344";
+			texts[i].style.textShadow = "0 0 25px #102344, 0 0 25px #102344, 0 0 25px #102344";
 		}
+		setTimeout(add_transition2,500);
 	}
 	else if (dark == "light") {
 		document.getElementById("mode").innerHTML = "Dark Mode";
 		var projects = document.getElementsByClassName("projects");
+		var texts = document.getElementsByClassName("hovertext");
 		for (var i = 0; i < projects.length; i++) {
+			projects[i].style.transitionDuration = "0.5s";
 			projects[i].style.backgroundColor = "#e4f8ff";
+			texts[i].style.textShadow = "0 0 25px #e4f8ff, 0 0 25px #e4f8ff, 0 0 25px #e4f8ff";
 		}
+		setTimeout(add_transition2,500);
 	}
 	document.body.classList.toggle("darkmode");
 	document.body.classList.toggle("lightmode");
 }
 
 function load() {
-	// $("#wrapper").fadeOut(100);
 	document.body.style.transitionDuration = "0s";
 	document.getElementById("wrapper").style.transitionDuration = "0s";
 	if (dark == "dark") {
 		document.body.classList.toggle("darkmode");
 		document.getElementById("mode").innerHTML = "Light Mode";
 		var projects = document.getElementsByClassName("projects");
+		var texts = document.getElementsByClassName("hovertext");
 		for (var i = 0; i < projects.length; i++) {
 			projects[i].style.backgroundColor = "#102344";
+			texts[i].style.textShadow = "0 0 25px #102344, 0 0 25px #102344, 0 0 25px #102344";
 		}
 	}
 	else if (dark == "light") {
 		document.body.classList.toggle("lightmode");
 		document.getElementById("mode").innerHTML = "Dark Mode";
 		var projects = document.getElementsByClassName("projects");
+		var texts = document.getElementsByClassName("hovertext");
 		for (var i = 0; i < projects.length; i++) {
 			projects[i].style.backgroundColor = "#e4f8ff";
+			texts[i].style.textShadow = "0 0 25px #e4f8ff, 0 0 25px #e4f8ff, 0 0 25px #e4f8ff";
 		}
 	}
 	setTimeout(function() {$("#wrapper").fadeTo(600, 1);},100,"linear");
@@ -89,4 +100,25 @@ function add_transition() {
 	document.body.style.transitionDuration = "0.5s";
 	document.getElementById("wrapper").style.transitionDuration = "0.5s";
 	// document.getElementById("wrapper").style.display = "block";
+}
+
+function add_transition2() {
+	var projects = document.getElementsByClassName("projects");
+	for (var i = 0; i < projects.length; i++) {
+		projects[i].style.transitionDuration = "0.2s"
+	}
+}
+
+function show_text(id) {
+	document.getElementById(`${id}_light`).style.filter = "blur(10px)";
+	// document.getElementById(`${id}_light`).style.transitionDuration = "inherit";
+	document.getElementById(`${id}_dark`).style.filter = "blur(10px)";
+	// document.getElementById(`${id}_dark`).style.transitionDuration = "inherit";
+}
+
+function hide_text(id) {
+	document.getElementById(`${id}_light`).style.filter = "none";
+	// document.getElementById(`${id}_light`).style.transitionDuration = "inherit";
+	document.getElementById(`${id}_dark`).style.filter = "none";
+	// document.getElementById(`${id}_dark`).style.transitionDuration = "inherit";
 }
