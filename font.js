884195796d7c88950c7838c79a891be92b9b3f75
KeeMeng <KeeMeng@users.noreply.github.com>
2021-08-16 01:49:39 +0800

    Update font.js

--- font.js
+++ font.js
@@ -1,3 +1,12 @@
 function start() {
-	document.getElementById("output").innerHTML = document.getElementById("input").value;
+	var text = document.getElementById("input").value;
+	var output_text = "";
+		for (var i = 0; i < text.length; i++) {
+			if (text[i] == text[i].toLowerCase()) {
+				output_text += text[i].toUpperCase();
+			} else {
+				output_text += text[i].toLowerCase();
+			}
+		}
+	document.getElementById("output").innerHTML = output_text;
 }
