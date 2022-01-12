8b770500e103ac819f599c7685f7daad2b9fbe25
Kee Meng <49086005+KeeMeng@users.noreply.github.com>
2022-01-12 22:19:08 +0800

    collapse width

--- cv.js
+++ cv.js
@@ -170,8 +170,8 @@ function summary() {
 			elements[i].style.display = "none";
 		}
 		for (var i = 0; i < data.length; i++) {
-			document.getElementById(`card${i}`).style.padding = "14px";
-			document.getElementById(`card${i}`).style.width = "calc(81.5% - 91px)";
+			document.getElementById(`card${i}`).style.padding = "12px";
+			document.getElementById(`card${i}`).style.width = "calc(var(--width) - 72px)";
 			document.getElementById(`tag${i}`).style.bottom = "12px";
 			document.getElementById(`tag${i}`).style.right = "12px";
 		}
@@ -183,7 +183,7 @@ function summary() {
 		}
 		for (var i = 0; i < data.length; i++) {
 			document.getElementById(`card${i}`).style.padding = "24px";
-			document.getElementById(`card${i}`).style.width = "calc(81.5% - 111px)";
+			document.getElementById(`card${i}`).style.width = "calc(var(--width) - 96px)";
 			document.getElementById(`tag${i}`).style.bottom = "24px";
 			document.getElementById(`tag${i}`).style.right = "24px";
 		}
